742c4a4542dd035d911a84d58ab0fcff804e3520
root <tony.hughes@orange.fr>
2020-11-14 16:56:45 +0100

    first commit

---
+++ carte/carte.php
@@ -0,0 +1,40 @@
+<?php
+namespace Carte;
+?>
+<div id="nom"
+    style="color: black; font-weight: bold; font-size: 14px; text-shadow: 1px 1px 1px rgb(255, 255, 255); border: 1px solid white; background-color: rgba(255,255,255,0.85); z-index:10;">
+</div>
+<?php
+
+class Carte {
+    private $communePoly = [];
+    private $communes = [];
+   
+    function __construct($communePoly) {
+        $this->communePoly = $communePoly;
+    }
+   
+    function show() {
+        $this->openSVG();
+
+        $this->showPolys();
+
+        $this->closeSVG();
+    }
+    private function openSVG() {
+        echo '<svg id="svgDiv" version="1.1" xmlns:bx="https://boxy-svg.com" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 600" enable-background="new 0 0 1000 600" xml:space="preserve">';
+    }
+    private function closeSVG() {
+        echo '</svg>';
+    }
+    private function showPolys() {
+        foreach ($this->communePoly as $key => $commune) {
+            echo '<polygon id="' . $key . '" name="' . $commune->nom . '" class="' . $commune->class . '" fill="' . $commune->fill . '"';
+            echo ' stroke="' . $commune->stroke . '" stroke_width="' . $commune->stroke_width . '"';
+            echo ' points="' . $commune->points . '"/>';
+          }
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+    define('ROOT', dirname(__DIR__));
+
+    require_once "./database/MysqlDatabase.php";
+
+    $db = new Core\Database\MysqlDatabase("carte");
+    $pdo = null;
+    try {
+        $pdo = $db->getPDO();
+    } catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
+    }
+    // var_dump($pdo);
+    $query = "SELECT cc.*, c.nom as nom ";
+    $query = $query . " FROM `communecarte` cc ";
+    $query = $query . " JOIN commune c ON cc.commune = c.id";
+    $communesCarte = $db->query($query);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>RWF Carte Interactiv</title>
+  <link rel="stylesheet" href="./assets/CSS/carte.css">
+</head>
+<body >
+  <h1 style="text-align:center;">Rural Web Factory - Carte Intractive</h1>
+
+Hello
+<?php require ROOT . '/Carte/carte/carte.php'; 
+
+    $carte = new Carte\Carte($communesCarte);
+    $carte->show();
+?>
+
+  <script src="./assets/JS/carte.js" type="text/javascript"></script>
+  </body>
+</html>
